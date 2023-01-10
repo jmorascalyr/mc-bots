d3de56dddb82aeb2faee814b268e63c658b3aa6a
jmorascalyr <42879226+jmorascalyr@users.noreply.github.com>
2023-01-10 10:36:07 -0800

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -5,4 +5,4 @@ apk add docker docker-cli-compose git --allow-untrusted
 wget http://www.eicar.org/download/eicar.com -P /tmp
 git clone https://github.com/jmorascalyr/mc-bots.git
 cd mc-bots
-docker compose up -d
+docker compose up
