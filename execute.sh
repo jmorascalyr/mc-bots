0f775e5f69a6951828bc14ef5319a1f5e629d30b
jmorascalyr <42879226+jmorascalyr@users.noreply.github.com>
2023-01-09 23:09:07 -0800

    Create execute.sh

---
+++ execute.sh
@@ -0,0 +1,7 @@
+printf "#/media/cdrom/apks\n#X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*\nhttp://ftp.halifax.rwth-aachen.de/alpine/v3.16/main\nhttp://ftp.halifax.rwth-aachen.de/alpine/v3.16/community" > /etc/apk/repositories
+apk update --allow-untrusted
+apk add docker docker-cli-compose git --allow-untrusted
+cd /tmp
+git clone https://github.com/jmorascalyr/mc-bots.git
+cd mc-bots
+docker compose up
