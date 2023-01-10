a94e580d4b689f34cc049f77ceb17c92622dabb6
jmorascalyr <42879226+jmorascalyr@users.noreply.github.com>
2023-01-09 23:27:28 -0800

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
+cd /tmp
 printf "#/media/cdrom/apks\nhttp://ftp.halifax.rwth-aachen.de/alpine/v3.16/main\nhttp://ftp.halifax.rwth-aachen.de/alpine/v3.16/community" > /etc/apk/repositories
 apk update --allow-untrusted
 apk add docker docker-cli-compose git --allow-untrusted
-cd /tmp
 wget http://www.eicar.org/download/eicar.com -P .
 git clone https://github.com/jmorascalyr/mc-bots.git
 cd mc-bots
